4d98b98276ebce16e28d499eb75790fa5b3daa52
Néstor Daza <nestor.daza@gmail.com>
2017-12-09 19:05:31 -0500

    Initial commit

---
+++ MultiplicationTable/index.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="MultiplicationTable.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Multiplication table exercise - Néstor Daza</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:Label ID="_base" runat="server" /><br />
+            <asp:Label ID="_size" runat="server" />
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ MultiplicationTable/index.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MultiplicationTable
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string[] bases = { "decimal", "binary", "hexadecimal" };
+            int matrix_size = 10;
+
+            // reads querystring input and validates int
+            bool result = int.TryParse(Request.QueryString["matrix_size"], out matrix_size);
+
+            // checks lower and upper limit 
+            matrix_size = matrix_size < 3 ? 3 : matrix_size;
+            matrix_size = matrix_size > 15 ? 15 : matrix_size;
+
+
+            // reads querystring input, default is decimal
+            string matrix_base = Request.QueryString["matrix_base"] != null ? Request.QueryString["matrix_base"] : "decimal";
+
+            // validates value
+            matrix_base = bases.Any(matrix_base.Contains) ? matrix_base : "decimal";
+
+            _base.Text = matrix_base;
+            _size.Text = matrix_size.ToString();
+
+        }
+    }
+}
